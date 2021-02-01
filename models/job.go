2a1e78cf427781d9107a2d155227355c245ae9e7
Samir Ettali <ettali.samir@gmail.com>
2021-02-01 14:59:46 +0100

    Changed job model and added validation

--- models/job.go
+++ models/job.go
@@ -2,15 +2,19 @@ package models
 
 type Job struct {
 	ID       string `json:"id"`
-	URL      string `json:"url"`
-	Interval uint64 `json:"interval"`
+	Name     string `json:"name" validate:"required,min=3,max=30"`
+	URL      string `json:"url" validate:"required,url"`
+	Interval uint64 `json:"interval" validate:"required,min=1"`
 	State    string `json:"-"`
-	Email    string `json:"email"`
+	Email    string `json:"email" validate:"required,email"`
+	Active   bool   `json:"active" validate:"required"`
 }
 
 type JobUpdate struct {
-	URL      *string `json:"url"`
-	Interval *uint64 `json:"interval"`
-	Email    *string `json:"email"`
+	URL      *string `json:"url" validate:"url"`
+	Name     *string `json:"name" validate:"min=3,max=30"`
+	Interval *uint64 `json:"interval" validate:"min=1"`
+	Email    *string `json:"email" validate:"email"`
 	State    *string `json:"-"`
+	Active   *bool   `json:"active"`
 }
